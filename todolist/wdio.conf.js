a03747ee75dd69b197915f398cc0a906f5df0c06
Acuña <micahl1981@gmail.com>
2022-08-04 15:01:05 -0300

    test para verificar tabla filtrada

--- todolist/wdio.conf.js
+++ todolist/wdio.conf.js
@@ -5,7 +5,7 @@ exports.config = {
         logLevel: "error", // [optional] error | verbose | silent, default: "error"
         url: "index.html", // [mandatory] {string} name of your bootstrap html file. If your server autoredirects to a 'domain:port/'-like root url, use empty string ''
         skipInjectUI5OnStart: false, // [optional] {boolean}, default: false; true when UI5 is not on the start page, you need to later call <wdioUI5service>.injectUI5() manually
-        waitForUI5Timeout: 15000 // [optional] {number}, default: 15000; maximum waiting time in milliseconds while checking for UI5 availability
+        waitForUI5Timeout: 40000 // [optional] {number}, default: 15000; maximum waiting time in milliseconds while checking for UI5 availability
     },
     //
     // ====================
@@ -111,7 +111,7 @@ exports.config = {
     baseUrl: 'http://localhost:8080',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 30000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
--- todolist/webapp/test/e2e/MasterJourney.test.js
+++ todolist/webapp/test/e2e/MasterJourney.test.js
@@ -1,18 +1,50 @@
 const { wdi5 } = require("wdio-ui5-service")
 const Master = require("./pageObjects/Master")
 
-describe("Master journey: ", () => {
+describe("The master page: ", () => {
+    const oListSelector = {
+        selector: {
+            interaction: "root",
+            controlType: "sap.m.List",
+            viewName: Master._viewName
+        }
+    }
+
+    const oSearchFieldSelector = {
+        selector: {
+            // controlType: "sap.m.Searchfield",
+            id: "searchField",
+            viewName: Master._viewName
+        }
+    }
+
     before(async () => {
         await Master.open()
     })
 
     it("should have the right title", async () => {
-        const title = await browser.getTitle()
-        expect(title).toEqual("To-Do List!")
+        const sTitle = await browser.getTitle()
+        expect(sTitle).toEqual("To-Do List!")
     })
 
     //should have a test that verifies that the list has items
+    it("should have a list with at least 3 items", async () => {
+        const oList = await browser.asControl(oListSelector);
+        const aListItems = await oList.getItems(true);
+        expect(aListItems.length).toBeGreaterThanOrEqual(3)
+    })
+
     //should have a test that verifies that the list can be filtered
+    it("should have a searchfield that filters the list", async () => {
+        const oSearchfield = await browser.asControl(oSearchFieldSelector);
+        await oSearchfield.setValue('agua');
+        await oSearchfield.fireLiveChange();
+
+        const oList = await browser.asControl(oListSelector);
+        const aListItems = await oList.getItems(true);
+        expect(aListItems.length).toBe(1);
+    })
+
     //should have a test that verifies that the list can be sorted
     //should have a test that verifies that we can add a new task
     //should have a test that verifies that we can check an item of the list and change it's state
