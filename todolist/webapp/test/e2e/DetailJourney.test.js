13b977d071e40521241b7ac04414c8d37f9e4f1c
Acuña <micahl1981@gmail.com>
2022-08-11 17:38:37 -0300

    Test en DetailJourney - navegación al item y edicion

--- todolist/webapp/test/e2e/DetailJourney.test.js
+++ todolist/webapp/test/e2e/DetailJourney.test.js
@@ -1,12 +1,103 @@
 const { wdi5 } = require("wdio-ui5-service")
+const Master = require("./pageObjects/Master")
 const Detail = require("./pageObjects/Detail")
+const { Selector } = require("./utils/MasterSelectors")
+let oSelector;
 
 describe("Detail journey: ", () => {
     before(async () => {
+        await Master.open() // We first run this command to 
         await Detail.open()
+        oSelector = await Selector(Master._viewName);
     })
 
-    //should have a test that verifies that we can edit an item
+    const oEditBtnSelector = {
+        selector: {
+            // controlType: "sap.m.Searchfield",
+            id: "edit",
+            viewName: Detail._viewName
+        }
+    }
+
+    const oInputTitleSelector = {
+        selector: {
+            interaction: "root",
+            // controlType: "sap.m.Input",
+            id: "inputDetail",
+            viewName: Detail._viewName
+        }
+    }
+
+    const oInputContentSelector = {
+        selector: {
+            interaction: "root",
+            // controlType: "sap.m.Input",
+            id: "textAreaDetail",
+            viewName: Detail._viewName
+        }
+    }
+
+    const oComboboxSelector = {
+        selector: {
+            interaction: "root",
+            // controlType: "sap.m.Combobox",
+            id: "slTypeDetail",
+            viewName: Detail._viewName
+        }
+    }
+
+    const oComboboxItemToSelectSelector = {
+        selector: {
+            controlType: "sap.m.StandardListItem",
+            viewName: Detail._viewName,
+            properties: {
+                title: "Critical - 3"
+            },
+            searchOpenDialogs: true
+        }
+    }
+
+    const oSaveEditBtnSelector = {
+        selector: {
+            id: "saveBtn",
+            viewName: sViewName
+        }
+    }
+
+    it("should be allowed to navigate to a detailed view", async () => {
+        const aItems = await browser.allControls(oItemToNavigateSelector)
+        await aItems[0].firePress()
+        const sUrl = await browser.getUrl()
+        const bNavigationOcurred = sUrl.includes('412790bd-390a-4626-ae32-ef3e7e3ccd0a');
+        expect(bNavigationOcurred).toBeTruthy();
+    })
+
+    //should have a test that verifies that we can edit an item    
+    it("should have an edit button", async () => {
+        const oEditBtn = await browser.asControl(oEditBtnSelector);
+        oEditBtn.press();
+        expect(oEditBtn._domId).toBeTruthy();
+    })
+
+    it("should allow edit", async () => {
+        // first, we get the components
+        const oInputTitle = await browser.asControl(oInputTitleSelector);
+        const oInputContent = await browser.asControl(oInputContentSelector);
+        const oCombobox = await browser.asControl(oComboboxSelector);
+        // second, we set text and select options
+        await oInputTitle.setValue('We edited this item with wdio!');
+        await oInputContent.setValue('We edited this content with webdriverio!');
+        await oCombobox.open();
+        const oComboboxItem = await browser.asControl(oComboboxItemToSelectSelector);
+        await oComboboxItem.press();
+
+        // we confirm that we want to add this new task
+        const oConfirmButton = await browser.asControl(oSaveEditBtnSelector);
+        await oConfirmButton.press();
+
+        // TODO - Verify that the item has been edited
+    })
+
     //should have a test that verifies that we can delete an item
     //should have a test that verifies that we can close the detail page
 
--- todolist/webapp/test/e2e/MasterJourney.test.js
+++ todolist/webapp/test/e2e/MasterJourney.test.js
@@ -10,98 +10,91 @@ describe("The master page: ", async () => {
         oSelector = await Selector(Master._viewName);
     })
 
-    it("should have the right title", async () => {
-        const sTitle = await browser.getTitle()
-        expect(sTitle).toEqual("To-Do List!")
-    })
+    // it("should have the right title", async () => {
+    //     const sTitle = await browser.getTitle()
+    //     expect(sTitle).toEqual("To-Do List!")
+    // })
 
-    it("should have a list with at least 3 items", async () => {
-        const oList = await browser.asControl(oSelector.oListSelector);
-        const aListItems = await oList.getItems(true);
-        expect(aListItems.length).toBeGreaterThanOrEqual(3)
-    })
+    // it("should have a list with at least 3 items", async () => {
+    //     const oList = await browser.asControl(oSelector.oListSelector);
+    //     const aListItems = await oList.getItems(true);
+    //     expect(aListItems.length).toBeGreaterThanOrEqual(3)
+    // })
 
-    it("should have a searchfield that filters the list", async () => {
-        const oSearchfield = await browser.asControl(oSelector.oSearchFieldSelector);
-        await oSearchfield.setValue('agua');
-        await oSearchfield.fireLiveChange();
+    // it("should have a searchfield that filters the list", async () => {
+    //     const oSearchfield = await browser.asControl(oSelector.oSearchFieldSelector);
+    //     await oSearchfield.setValue('agua');
+    //     await oSearchfield.fireLiveChange();
 
-        const oList = await browser.asControl(oSelector.oListSelector);
-        const aListItems = await oList.getItems(true);
-        expect(aListItems.length).toBe(1);
+    //     const oList = await browser.asControl(oSelector.oListSelector);
+    //     const aListItems = await oList.getItems(true);
+    //     expect(aListItems.length).toBe(1);
 
-        await oSearchfield.setValue('');
-        await oSearchfield.fireLiveChange();
-    })
+    //     await oSearchfield.setValue('');
+    //     await oSearchfield.fireLiveChange();
+    // })
 
-    it("should have a sort dialog that allows list sorting", async () => {
-        // first, we search and click the dialog button
-        const oButton = await browser.asControl(oSelector.oSortButtonSelector);
-        await oButton.press();
-        // second, we search the dialog, get it's buttons and trigger the sort event
-        const oSortDialog = await browser.asControl(oSelector.oSortDialogSelector);
-        const oDialogButtons = await oSortDialog.getButtons();
-        await oDialogButtons[0].firePress()
-        // third, we check if the list was sorted
-        const oListAfter = await browser.asControl(oSelector.oListSelector);
-        const aListItemsAfter = await oListAfter.getItems();
-        const aItemTextsAfter = await getListItemTitleTexts(aListItemsAfter);
-        const bIsTheListSortedInAscendingOrder = await checkAscendingOrder(aItemTextsAfter);
-        expect(bIsTheListSortedInAscendingOrder).toBeTruthy();
-    })
+    // it("should have a sort dialog that allows list sorting", async () => {
+    //     // first, we search and click the dialog button
+    //     const oButton = await browser.asControl(oSelector.oSortButtonSelector);
+    //     await oButton.press();
+    //     // second, we search the dialog, get it's buttons and trigger the sort event
+    //     const oSortDialog = await browser.asControl(oSelector.oSortDialogSelector);
+    //     const oDialogButtons = await oSortDialog.getButtons();
+    //     await oDialogButtons[0].firePress()
+    //     // third, we check if the list was sorted
+    //     const oListAfter = await browser.asControl(oSelector.oListSelector);
+    //     const aListItemsAfter = await oListAfter.getItems();
+    //     const aItemTextsAfter = await getListItemTitleTexts(aListItemsAfter);
+    //     const bIsTheListSortedInAscendingOrder = await checkAscendingOrder(aItemTextsAfter);
+    //     expect(bIsTheListSortedInAscendingOrder).toBeTruthy();
+    // })
 
-    it("should have a dialog to add a new task", async () => {
-        const oButton = await browser.asControl(oSelector.oAddButtonSelector);
-        await oButton.press();
-        const oAddTaskDialog = await browser.asControl(oSelector.oAddTaskDialogSelector);
-        expect(oAddTaskDialog).toBeTruthy();
-    })
+    // it("should have a dialog to add a new task", async () => {
+    //     const oButton = await browser.asControl(oSelector.oAddButtonSelector);
+    //     await oButton.press();
+    //     const oAddTaskDialog = await browser.asControl(oSelector.oAddTaskDialogSelector);
+    //     expect(oAddTaskDialog).toBeTruthy();
+    // })
 
-    it("should allow you to add a new task", async () => {
-        const oCreateButton = await browser.asControl(oSelector.oCreateButtonSelector);
-        const oInputTitle = await browser.asControl(oSelector.oTitleInputSelector);
-        const oInputContent = await browser.asControl(oSelector.oContentInputSelector);
-        const oCombobox = await browser.asControl(oSelector.oComboboxSelector);
+    // it("should allow you to add a new task", async () => {
+    //     // first, we get the components
+    //     const oCreateButton = await browser.asControl(oSelector.oCreateButtonSelector);
+    //     const oInputTitle = await browser.asControl(oSelector.oTitleInputSelector);
+    //     const oInputContent = await browser.asControl(oSelector.oContentInputSelector);
+    //     const oCombobox = await browser.asControl(oSelector.oComboboxSelector);
 
-        // we set text and select options
-        await oInputTitle.setValue('This is a test title');
-        await oInputContent.setValue('This is a test content');
-        await oCombobox.open();
-        const oComboboxItem = await browser.asControl(oSelector.oComboboxItemToSelectSelector);
-        await oComboboxItem.press();
-        await oCreateButton.press();
+    //     // second, we set text and select options
+    //     await oInputTitle.setValue('This is a test title');
+    //     await oInputContent.setValue('This is a test content');
+    //     await oCombobox.open();
+    //     const oComboboxItem = await browser.asControl(oSelector.oComboboxItemToSelectSelector);
+    //     await oComboboxItem.press();
+    //     await oCreateButton.press();
 
-        // we confirm that we want to add this new task
-        const oConfirmButton = await browser.asControl(oSelector.oConfirmButtonSelector);
-        await oConfirmButton.press();
+    //     // we confirm that we want to add this new task
+    //     const oConfirmButton = await browser.asControl(oSelector.oConfirmButtonSelector);
+    //     await oConfirmButton.press();
 
-        // we check if the list has the new item
-        const oNewItemTitle = await browser.asControl(oSelector.oExampleNewItemSelector);
-        const sNewItemDomId = oNewItemTitle._domId;
-        expect(sNewItemDomId).toBeTruthy();
-    })
+    //     // we check if the list has the new item
+    //     const oNewItemTitle = await browser.asControl(oSelector.oExampleNewItemSelector);
+    //     const sNewItemDomId = oNewItemTitle._domId;
+    //     expect(sNewItemDomId).toBeTruthy();
+    // })
 
-    it("should allow the status of a task to be modified", async () => {
-        // We bring all checkboxes
-        const aCheckBoxes = await browser.allControls(oSelector.oCheckBoxSelector);
-        // We choose the first one that has not been completed yet
-        const oItem = await getFirstUncompletedCheckbox(aCheckBoxes);
-        // We change its status
-        await oItem.item.setSelected(true);
-        await oItem.item.fireSelect();
-        // We check if the item status has been changed
-        const oCompletdItem = await aCheckBoxes.find(async (item) => {
-            return item._domId === oItem.domId;
-        })
-        const bIsCompleted = await oCompletdItem.getSelected();
-        expect(bIsCompleted).toBeTruthy();
-    })
-
-    it("should be allowed to navigate to a detailed view", async () => {
-        const aItems = await browser.allControls(oSelector.oItemToNavigateSelector)
-        await aItems[0].firePress()
-        const sUrl = await browser.getUrl()
-        const bNavigationOcurred = sUrl.includes('412790bd-390a-4626-ae32-ef3e7e3ccd0a');
-        expect(bNavigationOcurred).toBeTruthy();
-    })
+    // it("should allow the status of a task to be modified", async () => {
+    //     // We bring all checkboxes
+    //     const aCheckBoxes = await browser.allControls(oSelector.oCheckBoxSelector);
+    //     // We choose the first one that has not been completed yet
+    //     const oItem = await getFirstUncompletedCheckbox(aCheckBoxes);
+    //     // We change its status
+    //     await oItem.item.setSelected(true);
+    //     await oItem.item.fireSelect();
+    //     // We check if the item status has been changed
+    //     const oCompletdItem = await aCheckBoxes.find(async (item) => {
+    //         return item._domId === oItem.domId;
+    //     })
+    //     const bIsCompleted = await oCompletdItem.getSelected();
+    //     expect(bIsCompleted).toBeTruthy();
+    // })
 })
